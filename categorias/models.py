2c8e887f25cd20ff76b80d8c414e707c66b8f521
macknilan <rodolfougaldeochoa@gmail.com>
2017-10-04 23:28:08 -0500

    Mod Categoria guarda img nombre personalizado carpeta_e_img

--- categorias/models.py
+++ categorias/models.py
@@ -2,10 +2,26 @@
 from __future__ import unicode_literals
 
 import os
+from django.urls import reverse
+from django.utils.text import slugify
 from django.utils.crypto import get_random_string
+from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
+class SlugMixin(object):
+
+    def get_slug(self, text, model):
+        slug_text = slugify(text)
+        # count = 2
+        # fecha_ano = datetime.date.year()
+        slug = slug_text
+        if (model._default_manager.filter(slug=slug).exists()):
+            raise ValidationError(_('ESTO ES UN MENSAJE DE PRUEBA - ERROR ESTO ESTA REPETIDO'))
+        # while(model._default_manager.filter(slug=slug).exists()):
+        #     slug = '{0}-{1}'.format(slug_text, count)
+        return slug
 
 
 def change_file_name(self, imagefilename):
@@ -19,7 +35,7 @@ def change_file_name(self, imagefilename):
     return os.path.join('fotos_categoria', imagefilename)
 
 
-class Categoria(models.Model):
+class Categoria(SlugMixin, models.Model):
     CAT_M = (
         ('ninguno', 'Ninguno'),
         ('comedores', 'Comedores'),
@@ -30,6 +46,13 @@ class Categoria(models.Model):
     cat_mueble = models.CharField("Categoria del Mueble", max_length=10, choices=CAT_M, default='ninguno')
     imagen_categoria = models.ImageField("Foto de Categoria", upload_to=change_file_name, max_length=50)
     slug = models.CharField(max_length=140, blank=True, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('categorias:comedores', kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs):
+        self.slug = self.get_slug(self.cat_mueble, Categoria)
+        super(Categoria, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.cat_mueble
