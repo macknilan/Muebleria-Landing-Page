7f2183f77cfc0a506bfe29dc6317eef4afe16c47
macknilan <rodolfougaldeochoa@gmail.com>
2018-01-08 12:59:05 -0600

    Add urls de categorias

--- categorias/urls.py
+++ categorias/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
-from categorias.views import ComedoresListView
+from categorias.views import ComedoresListView, CocinasListView, ClosetsListView, PuertasListView, BanosListView
 
 
 urlpatterns = [
     url(r'^comedores/$', ComedoresListView.as_view(), name='comedores'),
+    url(r'^cocinas/$', CocinasListView.as_view(), name='cocinas'),
+    url(r'^closets/$', ClosetsListView.as_view(), name='closets'),
+    url(r'^puertas/$', PuertasListView.as_view(), name='puertas'),
+    url(r'^banos/$', BanosListView.as_view(), name='banos'),
 ]
