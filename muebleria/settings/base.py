729f20d872aa42e17a877a3e004d4874af5b4480
macknilan <rodolfougaldeochoa@gmail.com>
2019-03-19 15:57:19 -0600

    Mod. a -nav- para que se transparente

--- muebleria/settings/base.py
+++ muebleria/settings/base.py
@@ -91,16 +91,16 @@ DATABASES = {
 
 """   CACHE   """
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.environ['CACHELOCATION'],
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "PASSWORD": os.environ['CACHEPSWD'],
-        }
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": os.environ['CACHELOCATION'],
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#             "PASSWORD": os.environ['CACHEPSWD'],
+#         }
+#     }
+# }
 
 CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = 60
